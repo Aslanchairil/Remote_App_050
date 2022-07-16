29a44b0749e92a2c69d298403bf6ce5a3356fe04
ASLAN CHAIRIL SYAWAL <0120200005@polman.astra.ac.id>
2022-07-17 00:46:26 +0700

    ppp

---
+++ app/src/main/java/com/example/remoteapp_05/TambahDataFragment.java
@@ -0,0 +1,48 @@
+package com.example.remoteapp_05;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ToggleButton;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class TambahDataFragment extends Fragment {
+    ImageView mImage_off,mImage_on;
+    ToggleButton mToggleButton;
+
+    @Override
+    public void onCreate(Bundle saveInstanceState){
+        Bundle savedInstanceState = new Bundle();
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.tambah_data, container, false);
+        mImage_off = v.findViewById(R.id.lamp_off);
+        mImage_on = v.findViewById(R.id.lamp_on);
+        mToggleButton = v.findViewById(R.id.switch_button);
+
+        mToggleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mToggleButton.isChecked()) {
+                    mImage_off.setVisibility(View.VISIBLE);
+                    mImage_on.setVisibility(View.GONE);
+
+                } else {
+                    mImage_on.setVisibility(View.VISIBLE);
+                    mImage_off.setVisibility(View.GONE);
+                }
+            }
+        });
+        return v;
+    }
+}
